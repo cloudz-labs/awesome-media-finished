165a04c673d4264121a17addc1221219267b6efa
1000jaeh <1000jaeh@gmail.com>
2018-04-03 13:43:59 +0900

    add spring-cloud-service

---
+++ awesome-media-discvoery-service/src/main/java/com/awesome/AwesomeMediaDiscvoeryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeMediaDiscvoeryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeMediaDiscvoeryServiceApplication.class, args);
+	}
+}
